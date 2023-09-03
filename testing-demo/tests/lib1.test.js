f6e0131efb54c25b77d8c0ccdcdd6281c657520c
RashanWeerasinghe <malithrashan03@gmail.com>
2023-09-03 22:09:26 +0530

    grouping tests

--- testing-demo/tests/lib1.test.js
+++ testing-demo/tests/lib1.test.js
@@ -2,17 +2,26 @@ const lib = require("../lib");
 
 test("first test", () => {});
 
-test("absolute-should return positive number if input is positive", () => {
-  const result = lib.absolute(1);
-  expect(result).toBe(1);
+describe("absolute", () => {
+  it("should return positive number if input is positive", () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+
+  it("should return negative  number if input is negative", () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it("should return zero  number if input is zero", () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
 
-test("absolute-should return negative  number if input is negative", () => {
-  const result = lib.absolute(-1);
-  expect(result).toBe(1);
-});
-
-test("absolute-should return zero  number if input is zero", () => {
-  const result = lib.absolute(0);
-  expect(result).toBe(0);
+describe("greet", () => {
+  it("should return greeting message", () => {
+    const result = lib.greet("Rashan");
+    expect(result).toBe("Welcome Rashan");
+  });
 });
