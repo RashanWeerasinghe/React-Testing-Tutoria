c6cc894ef3358d5d2a5156e58c64104961c389a5
RashanWeerasinghe <malithrashan03@gmail.com>
2023-09-03 21:39:49 +0530

    some changes testing

--- testing-demo/tests/lib1.test.js
+++ testing-demo/tests/lib1.test.js
@@ -1,3 +1,18 @@
-test("first test", () => {
-  throw new Error("something is failed");
+const lib = require("../lib");
+
+test("first test", () => {});
+
+test("absolute-should return positive number if input is positive", () => {
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
+});
+
+test("absolute-should return negative  number if input is negative", () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test("absolute-should return zero  number if input is zero", () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
 });
