6af25992e801aa17229395849d3b9a338ff8ab50
RashanWeerasinghe <malithrashan03@gmail.com>
2023-09-06 21:47:15 +0530

    jest unit testing post http post request

--- to-do/controller/todo.controller.js
+++ to-do/controller/todo.controller.js
@@ -1,1 +1,5 @@
-exports.createTodo = () => {};
+const todoModel = require("../model/todo.model");
+exports.createTodo = (req, res, next) => {
+  todoModel.create(req.body);
+  res.status(201);
+};
--- to-do/tests/unit/todo.controller.test.js
+++ to-do/tests/unit/todo.controller.test.js
@@ -1,7 +1,29 @@
 const TodoController = require(".../../../controller/todo.controller");
 
+const todoModel = require("../../model/todo.model");
+
+const newTodo = require("../../data/fakeData.json");
+const httpMocks = require("node-mocks-http");
+
+todoModel.create = jest.fn();
+
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe("TodoController.createTodo", () => {
   it("should have a createTodo function", () => {
     expect(typeof TodoController.createTodo).toBe("function");
+  });
+  it("should have todo.create", () => {
+    req.body = newTodo;
+    TodoController.createTodo(req, res, next);
+
+    expect(todoModel.create).toBeCalledWith(newTodo);
+
+    expect(res.statusCode).toBe(201);
   });
 });
